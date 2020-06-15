29a3010343527d9436495824c31718fbad4f7bb0
makula <makulajosh@gmail.com>
2020-06-15 20:21:08 +0300

    exercise

---
+++ exercise1.js
@@ -0,0 +1,48 @@
+// Declaring variable name
+var name;
+// Assigning value to name
+name = " ozzy queen"
+// printing name
+console.log(name);
+// print break line tag
+console.log("<br/>");
+// another variable declaration
+var name;
+// assgning string variable
+name = "ozzy hum"
+// print name
+console.log(name)
+// another variable decralation
+var name;
+// name is a string 
+name = "sumi mike ozyy"
+// print sumi mike ozzy
+console.log(name)
+// declare name variable
+var name;
+// assign ozzy bp to name
+name = " ozzy bp"
+// print value of name
+console.log(name)
+// asssign name to value Bridget ozzy
+name = " Bridget ozzy"
+// out put value of name
+console.log(name)
+// declare variable age
+var age;
+//declare variable name
+var name;
+// assign age a value 50 and name a value mirembe agaba kwesi
+age = 50;
+name = "mirembe agaba kwesi"
+// output diffent varible on the same line with some concantination
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+
+// variable declaration x
+var x;
+// output variable x
+document.write(x);
+// assign variable undeclared
+var x = undefined;
+// output x
+console.log(x);
